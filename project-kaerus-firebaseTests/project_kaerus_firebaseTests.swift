7a590adf65d9594d1471989ddac5018207c887d1
Brandon Chen <brandonchenjiacheng@gmail.com>
2016-07-15 11:46:29 -0700

    Initial Commit

---
+++ project-kaerus-firebaseTests/project_kaerus_firebaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  project_kaerus_firebaseTests.swift
+//  project-kaerus-firebaseTests
+//
+//  Created by Brandon Chen on 7/15/16.
+//  Copyright © 2016 Brandon Chen. All rights reserved.
+//
+
+import XCTest
+@testable import project_kaerus_firebase
+
+class project_kaerus_firebaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
